ffa42d3ee7b3f5859615881506c21177f801bdf4
Jonas Schön <js@gefyra.de>
2025-09-09 10:08:17 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve as r } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig(({ command }) => {
   const isBuild = command === 'build'
   return {
     base: isBuild ? '/ISiK-Questionnaire-Tooling-Demo/' : '/',
+    build: {
+      rollupOptions: {
+        // Explicitly include multi-page entries
+        input: {
+          main: r(__dirname, 'index.html'),
+          resolve: r(__dirname, 'resolve.html'),
+        }
+      }
+    }
   }
 })
